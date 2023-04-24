635bbff6b588d78c718b1bc5f84bd60bf9e4a06c
devjoao <victorisilva428@gmail.com>
2023-04-23 21:32:40 -0300

    inital commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
 import 'dart:math';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+
+import 'listpassword.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,9 +14,44 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
+class Password {
+  final String name;
+  final String password;
+
+  Password({required this.name, required this.password});
+
+  static Password fromMap(Map<String, dynamic> map) {
+    return Password(
+      name: map['name'],
+      password: map['password'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'password': password,
+    };
+  }
+}
+
 class _HomeState extends State<Home> {
+  String _nome = '';
   String _senha = '';
   int _comprimentoSenha = 8;
+  TextEditingController _nameController = TextEditingController();
+
+  late SharedPreferences _prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    _initPrefs();
+  }
+
+  Future<void> _initPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
+  }
 
   String gerarSenhaAleatoria(int comprimento) {
     const chars =
@@ -21,6 +59,13 @@ class _HomeState extends State<Home> {
     final random = Random.secure();
     return List.generate(
         comprimento, (index) => chars[random.nextInt(chars.length)]).join();
+  }
+
+  Future<void> _savePassword(String name, String password) async {
+    final passwordList = _prefs.getStringList('passwords') ?? [];
+    final passwordObj = Password(name: name, password: password);
+    passwordList.add(jsonEncode(passwordObj.toJson()));
+    await _prefs.setStringList('passwords', passwordList);
   }
 
   @override
@@ -31,7 +76,7 @@ class _HomeState extends State<Home> {
         title: Text('Gerador de Senha'),
         backgroundColor: Colors.black,
       ),
-      body: Center(
+      body: SingleChildScrollView(child: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -82,7 +127,7 @@ class _HomeState extends State<Home> {
               ),
               SizedBox(height: 20),
               GestureDetector(
-                onTap: (){
+                onTap: () {
                   Clipboard.setData(ClipboardData(text: _senha));
                   setState(() {
                     Fluttertoast.showToast(
@@ -92,19 +137,117 @@ class _HomeState extends State<Home> {
                       textColor: Colors.white,
                     );
                   });
-
                 },
                 child: Text(
-                _senha,
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-              ),),
-              SizedBox(height: 8,),
-              if(!_senha.isEmpty)
-                Text('Clique na senha para Copia.')
-
+                  _senha,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              if (!_senha.isEmpty) Text('Clique na senha para Copia.'),
+              SizedBox(height: 16),
+              if (!_senha.isEmpty)
+                SizedBox(
+                  height: 60,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    child: Text('Salvar senha'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text(
+                            "Salvar senha",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                _senha,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextField(
+                                controller: _nameController,
+                                decoration: InputDecoration(
+                                  labelText: "Nome da senha",
+                                  labelStyle: TextStyle(color: Colors.black),
+                                  focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.black),
+                                  ),
+                                  enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.black),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          actions: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: Text("Cancelar"),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                _savePassword(_senha, _nameController.text);
+                                Navigator.of(context).pop();
+                              },
+                              child: Text("Salvar"),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
             ],
           ),
         ),
+      ),),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          final passwordList = _prefs.getStringList('passwords');
+          if (passwordList == null || passwordList.isEmpty) {
+            Fluttertoast.showToast(
+              msg: "Voce nao tem senha guardada",
+              gravity: ToastGravity.BOTTOM,
+              backgroundColor: Colors.grey[700],
+              textColor: Colors.white,
+            );
+          } else {
+            // Há senhas salvas, navegue para a lista
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PasswordList()),
+            );
+          }
+        },
+        icon: Icon(Icons.lock),
+        label: Text('Senhas salvas'),
+        backgroundColor: Colors.black,
       ),
     );
   }
---
+++ lib/listpassword.dart
@@ -0,0 +1,117 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'home.dart';
+
+class PasswordList extends StatefulWidget {
+  const PasswordList({Key? key}) : super(key: key);
+
+  @override
+  State<PasswordList> createState() => _PasswordListState();
+}
+
+class _PasswordListState extends State<PasswordList> {
+  late SharedPreferences _prefs;
+  List<Password> _passwords = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _initPrefs();
+  }
+
+  Future<void> _initPrefs() async {
+    _prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _passwords = _prefs.getStringList('passwords')?.map((passwordJson) {
+            final passwordObj = Password.fromMap(jsonDecode(passwordJson));
+            return passwordObj;
+          }).toList() ??
+          [];
+      print(_passwords);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Lista de Senhas'),
+        backgroundColor: Colors.black,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: _passwords.length,
+          itemBuilder: (context, index) {
+            final password = _passwords[index];
+            final name = password.name;
+            final pass = password.password;
+            return GestureDetector(
+              onTap: () {
+                Clipboard.setData(ClipboardData(text: name));
+                setState(() {
+                  Fluttertoast.showToast(
+                    msg: "Senha copiada para a área de transferência: $name",
+                    gravity: ToastGravity.BOTTOM,
+                    backgroundColor: Colors.grey[700],
+                    textColor: Colors.white,
+                  );
+                });
+              },
+              child: PasswordItem(name: pass, password: name),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class PasswordItem extends StatefulWidget {
+  final String name;
+  final String password;
+
+  const PasswordItem({Key? key, required this.name, required this.password})
+      : super(key: key);
+
+  @override
+  State<PasswordItem> createState() => _PasswordItemState();
+}
+
+class _PasswordItemState extends State<PasswordItem> {
+  bool _isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          '${widget.name}:',
+          style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        const SizedBox(width: 8),
+        Expanded(
+          child: Text(
+            _isPasswordVisible ? widget.password : '*******',
+            style: const TextStyle(fontSize: 16),
+          ),
+        ),
+        IconButton(
+          icon: Icon(
+              _isPasswordVisible ? Icons.visibility_off : Icons.visibility),
+          onPressed: () {
+            setState(() {
+              _isPasswordVisible = !_isPasswordVisible;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
